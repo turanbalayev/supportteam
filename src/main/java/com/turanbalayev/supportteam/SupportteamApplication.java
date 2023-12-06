50b1e66588a8e6279540d6ac2e64c4f07e152d0a
Turan Balayev <turanbalayev96@gmail.com>
2023-12-06 19:16:55 +0400

    First commit

---
+++ src/main/java/com/turanbalayev/supportteam/SupportteamApplication.java
@@ -0,0 +1,13 @@
+package com.turanbalayev.supportteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportteamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turanbalayev/supportteam/SupportteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turanbalayev.supportteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
